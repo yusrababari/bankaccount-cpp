c6decad59faee1351ce8fd65152854d830980360
yusra <yusra.babari@outlook.com>
2026-06-10 22:30:45 +0100

    changed number

--- bankaccount.cpp
+++ bankaccount.cpp
@@ -7,7 +7,7 @@ class BankAccount {
     public:
        string name;
        int balance;
-       int accountNumber;
+       int number;
        BankAccount(string accountName, int accountBalance,,int accountNumber){
         // initialisation of variables
         // variables in constructor have different names
