21fa61d15037771dc8233aba2d99a855bad078cf
yusra <yusra.babari@outlook.com>
2026-06-12 21:43:07 +0100

    made changes to constructor

--- bankaccount.cpp
+++ bankaccount.cpp
@@ -11,12 +11,14 @@ class BankAccount {
        BankAccount(string accountName, int accountBalance,int accountNumber){
         // initialisation of variables
         // variables in constructor have different names
-        name=accountNumber;
+        name=accountName;
         // variables comes before constructor
         balance = accountBalance;
         number= accountNumber;
        }
        // print variables
+       cout << name
+
 
 
          
